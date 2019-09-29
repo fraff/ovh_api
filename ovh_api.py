4cc4e6dbba8576cdad3757b08394012ccf656503
fraff <fraff@users.noreply.github.com>
2019-09-29 11:51:58 +0200

    Add files via upload

---
+++ ovh_api.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ovh_api, an Ansible module for accessing ovh api
+# Copyright (C) 2019, Francois Lallart <fraff@free.fr>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+
+DOCUMENTATION = '''
+---
+module: ovh_api
+author: Francois (fraff) Lallart
+short_description: Minimalist wrapper around OVH api and ovh python module
+description:
+    - Enable 
+requirements: [ "ovh" ]
+options:
+    path
+        required: true
+        description:
+            - https://api.ovh.com/console/{path}
+    method:
+        required: true
+        choices: ['GET', 'POST', 'PUT', 'DELETE']
+        aliases: ['action']
+        description:
+            - obvious
+    body:
+        required: false
+        default: 
+        description:
+            - use this body if required
+'''
+
+EXAMPLES = '''
+# basic
+- ovh_api: path="/cloud/project"
+- ovh_api:
+    path: "/ip/x.x.x.x%2f29/reverse"
+    method: "POST"
+    body: {'ipReverse': 'y.y.y.y', reverse: 'my.cool.reverse'}
+'''
+
+import os
+import sys
+
+try:
+    import ovh
+    import ovh.exceptions
+    from ovh.exceptions import APIError
+except ImportError:
+    print "failed=True msg='ovh required for this module'"
+    sys.exit(1)
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            path = dict(required=True),
+            method = dict(required=False, default='GET', choices=['GET', 'POST', 'PUT', 'DELETE'], aliases=['action']),
+            body = dict(required=False, default={}, type=dict, aliases=['data'])
+        ),
+        supports_check_mode = True
+    )
+
+    # Get parameters
+    path = module.params.get('path')
+    method = module.params.get('method').lower()
+    body = module.params.get('body')
+
+#    module.warn(method)
+#    print json.dumps({
+#        "time" : str(datetime.datetime.now())
+#    })
+
+
+    if module.check_mode:
+        module.fail_json(msg="Check Mode is not supported.")
+
+    # Connect to OVH API
+    client = ovh.Client()
+
+    mydict = {
+        'func': {'get': client.get, 'put': client.put, 'post': client.post, 'delete': client.delete},
+        'code': {'get': False, 'put': True, 'post': True, 'delete': True}
+    }
+
+    try:
+        result = mydict['func'][method](path, **body)
+        module.exit_json(changed=mydict['code'][method], result=result)
+        # TODO try to get http code from result
+
+    except APIError as apiError:
+        module.fail_json(changed=False, msg="OVH API Error: {0}".format(apiError))
+
+    # We should never reach here
+    module.fail_json(msg='Internal ovh_api module error')
+
+
+# import module snippets
+from ansible.module_utils.basic import *
+
+main()
