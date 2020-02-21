91a7ddaede282f18436cd1df2f99ef12a821752c
Francois Lallart <fraff@free.fr>
2020-02-21 11:47:45 +0100

    sync from https://github.com/ansible/ansible/pull/65566

--- ovh_api.py
+++ ovh_api.py
@@ -84,7 +84,17 @@ EXAMPLES = '''
     body: {'ipReverse': 'y.y.y.y', reverse: 'my.cool.reverse'}
 '''
 
-RETURN = '''
+RETURN = r'''
+result:
+    description: Raw response from Ovh API, content varies with API.
+    returned: success
+    type: dict
+    sample: {"firstname": "Francois", "name": "Lallart", "organisation": "@fraff", "language": "fr_FR", "city": "Lille", "country": "FR"}
+msg:
+    description: Module error message.
+    returned: failure
+    type: str
+    sample: "OVH API Error: Got an invalid (or empty) URL"
 '''
 
 import os
